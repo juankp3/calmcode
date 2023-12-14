c297cc27394fc8d073427c52cfb00fcb2e8ff713
Josias Olaya <josiasolaya2016@gmail.com>
2023-12-13 21:08:01 -0500

    update: numeros

--- josias.js
+++ josias.js
@@ -2,4 +2,9 @@ const holiwis = 'Josue ';
 const correo = 'josiasholiwis@gmail.com';
 const cambios = 'este es un nuevo cambio';
 const intento = 'Este es un nuevo intento';
+<<<<<<< HEAD
 const intento2 = '*-*  Este es un nuevo intento2-------';
+=======
+const intento2 = '*-*  Este es un nuevo intento2-------';
+aaaa
+>>>>>>> 6a23aea6cb24ff2346d02bcd82b374f6e5c6f744
